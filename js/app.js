e4175b02d070b5dd481d281135992ab4bf379649
westondombroski <westondombroski@gmail.com>
2017-02-01 22:11:26 -0800

    racing game styled

--- js/app.js
+++ js/app.js
@@ -1,7 +1,35 @@
-$(document).ready(function(e) {
-  $("div").keydown(function(){
-    $(this).animate({
-        marginLeft: "+=50px",
-    }, 1000 );
-});
+$(document).ready(function() {
+
+
+
+  $(window).keyup(function(e){
+    if (e.keyCode == 13){
+      $(".car1").animate({
+        marginLeft: "+=5px"
+      }, 20 );
+    }
+  });
+
+
+  $(window).keyup(function(e){
+    if (e.keyCode == 32){
+      $(".car2").animate({
+        marginLeft: "+=5px"
+      }, 20 );
+    }
+  });
+
+var car1 = $(".car1");
+var car2 = $(".car2");
+var car1Position = car1.position();
+var car2Position = car2.position();
+
+  if(car1Position.leftMargin === 900){
+    alert("Car1 Wins!!!!");
+  }
+
+  if(car2Position.left === 900){
+    alert("Car2 Wins!!!!");
+  }
+
 });
