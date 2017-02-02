32ba118b492d2df6e188ddbf8c6141b10e30257f
westondombroski <westondombroski@gmail.com>
2017-02-02 08:50:36 -0800

    it worksgit status

--- js/app.js
+++ js/app.js
@@ -1,32 +1,43 @@
 $(document).ready(function() {
 
+var car1counter = 0;
 
+/* -- race car 1 --*/
   $(window).keyup(function(e){
+
     if (e.keyCode == 13){
+      car1counter++;
+
       $(".car1").animate({
         marginLeft: "+=25px"
       }, 20 );
     }
   });
 
+/* -- car 1 wins--*/
   setInterval(function(){
-     if ($(".car1").css("marginLeft") == '1300px'){
+     if (car1counter > 50){
        $(".car1wins").removeClass("hidden");
      }
   }, 10);
 
-
+/* -- race car 2 --*/
   $(window).keyup(function(e){
     if (e.keyCode == 32){
+      car2counter++;
+
       $(".car2").animate({
-        marginLeft: "+=5px"
+        marginLeft: "+=25px"
       }, 20 );
     }
   });
 
+ var car2counter = 0;
+
+/* -- car 2 wins--*/
   setInterval(function(){
-     if ($(".car1").css("marginLeft") == '1300px'){
-       $(".car1wins").removeClass("hidden");
+     if (car2counter > 50){
+       $(".car2wins").removeClass("hidden");
      }
   }, 10);
 
